623fab98eb3d7bce8185393512eb124f7afb2239
zhangjiruir <1624694837@qq.com>
2019-11-20 19:10:21 +0800

    11.20

--- week9/day5/tode/index.js
+++ week9/day5/tode/index.js
@@ -1,8 +1,8 @@
-Vue.directive('focus',function(el,obj){
-    if(obj.value){
-        setTimeout(()=>{
+Vue.directive('focus', function (el, obj) {
+    if (obj.value) {
+        setTimeout(() => {
             el.focus()
-        },10)
+        }, 10)
     }
 })
 
@@ -26,18 +26,19 @@ new Vue({
             editable: false
         }],
         todo: '',
-        hash:'',// 用来存储当前路径的hash值
+        count:0,
+        hash: '',// 用来存储当前路径的hash值
     },
-    created(){
+    created() {
         this.hash = location.hash || '#/all';
-        window.addEventListener('hashchange',()=>{
+        window.addEventListener('hashchange', () => {
             this.hash = location.hash
         })
     },
     methods: {
         submit() {
             this.todo = this.todo.trim();
-            if (!this.todo)return;
+            if (!this.todo) return;
             let obj = {
                 id: Math.random(),
                 done: false,
@@ -47,11 +48,35 @@ new Vue({
             this.ary.unshift(obj);
             this.todo = '';
         },
-        change(obj){
-            obj.editable =!obj.editable
+        change(obj) {
+            obj.editable = !obj.editable
         },
-        del(n){
-            this.ary.splice(n,1)
+        del(n) {
+            this.ary.splice(n, 1)
         }
     },
+    computed: {
+        todoAry() {
+            // 未完成的事件各数  因为只要数组发生改变 count就要重新赋值
+            this.count = this.ary.filter(item=>!item.done).length
+            // 依赖于 ary  hash
+            switch (this.hash) {
+                case '#/all':
+                    // 若是全部列表 则把整个数字返回
+                    return this.ary
+                    break;
+
+                case '#/finished':
+                    // 若是完成列表 则返回 ary中 done属性是true的向
+                    return this.ary.filter(item => item.done)
+                    break;
+
+                case '#/unfinished':
+                    return this.ary.filter(item => !item.done)
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 })
